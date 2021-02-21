b568a0481b2b29a6021bc9c90eb62e240fcb0cf8
RaghuveerChebrolu <raghuveerchebrolu@gmail.com>
2021-02-21 19:27:12 +0530

    first commit

---
+++ src/main/java/mavenjyoraghu1/mavenjyoraghu1/App.java
@@ -0,0 +1,13 @@
+package mavenjyoraghu1.mavenjyoraghu1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
